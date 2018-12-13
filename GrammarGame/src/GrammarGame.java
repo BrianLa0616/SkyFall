acaf4c6c692af5b2ebcbf38d44e47975e90e29fe
brianla0616 <43937402+brianla0616@users.noreply.github.com>
2018-12-13 08:39:20 -0800

    Menu with rules

---
+++ GrammarGame/src/GrammarGame.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+import processing.core.PShape;
+
+public class GrammarGame {
+	
+	public static Menu menu;
+
+	
+	public static void main(String args[]) {
+		menu = new Menu();
+		
+
+	}
+}
---
+++ GrammarGame/src/Menu.java
@@ -0,0 +1,90 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import bla269.shapes.Rectangle;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Menu extends PApplet {
+
+	private Rectangle rules, play, back;
+	private boolean isRulePage;
+	private PImage img;
+	private JFrame window;
+
+	public Menu() {
+		PApplet.runSketch(new String[]{""}, this);
+		PSurfaceAWT surf = (PSurfaceAWT) this.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		window = (JFrame)canvas.getFrame();
+
+		window.setBounds(100, 100, 500, 500);
+		window.setMinimumSize(new Dimension(200,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		canvas.requestFocus();
+		isRulePage = false;
+		play = new Rectangle(150, 200, 200, 75);
+		play.setfill(0,180,255);
+		rules = new Rectangle(150, 325, 200, 75);
+		rules.setfill(0, 180, 255);
+		back = new Rectangle(25, 400, 50, 50);
+		back.setfill(0, 180, 255);
+
+	}
+	
+	public void setup() { 
+		  img = loadImage("back.png");
+
+	}
+
+	public void draw() {
+		background(127, 217, 255);
+
+		if (isRulePage) {
+//			back.draw(this);
+			image(img,27,400,width/10,height/10);
+			
+			textSize(50);
+			text("HOW TO PLAY", 80, 75);
+			textSize(20);
+			text("1. Read the sentence at the top of the screen", 40, 125);
+			text("2. View answer choices in each cloud", 40, 175);
+			text("3. Use arrow keys to move the basket", 40, 225);
+			text("4. Catch the correct term ", 40, 275);
+			text("5. Use left/right arrow to left/right", 40, 325);
+			text("6. Press p to pause", 40, 375);
+
+		} else {
+			rules.draw(this);
+			play.draw(this);
+			textSize(70);
+			text("SKYFALL", 105,125);
+			textSize(40);
+			text("PLAY", 200, 255);
+			text("RULES", 190, 380);
+		}
+	}
+
+	public void mousePressed() {
+		if (!isRulePage && rules.isPointInside(mouseX, mouseY)) {
+			isRulePage = true;
+		} else if (isRulePage && back.isPointInside(mouseX, mouseY)) {
+			isRulePage = false;
+		}
+		else if (!isRulePage && play.isPointInside(mouseX, mouseY)) {
+			
+			window.dispose();
+			
+			GameBoard gameboard = new GameBoard();
+			
+			
+
+		}
+	}
+
+}
